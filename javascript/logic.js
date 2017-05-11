e90fce4f0901fbcc04c90fa6a52e6074483d8ba8
Tony Storti <tonystorti@gmail.com>
2017-05-11 16:41:16 -0500

    deleted unnecessary code

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
    $("#getMessage").on("click", function(){  
       //API call for random Trump Quote
-      $("#test").html("test message");
       $.getJSON('https://api.whatdoestrumpthink.com/api/v1/quotes/random', function(json){
          $("#message").html(json.message);
          $("#quoteBlock").attr("style", "visibility:visible");
